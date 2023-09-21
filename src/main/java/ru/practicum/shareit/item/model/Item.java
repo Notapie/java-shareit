62723aac8d801a8bcbdacc2e49375a9a74305c53
notapie <notapie@yandex.ru>
2023-09-21 23:36:31 +0300

    feat: add Item struct

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,14 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.user.User;
+
+@Data
+@Builder(toBuilder = true)
 public class Item {
+    Integer id;
+    User owner;
+    String name;
+    String description;
 }
