863ae0abd3e025497f963266134c26eded28e73b
notapie <notapie@yandex.ru>
2023-09-24 23:30:37 +0300

    feat: add ItemController struct

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,43 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import java.util.Collection;
+
 @RestController
 @RequestMapping("/items")
+@Slf4j
+@RequiredArgsConstructor
 public class ItemController {
+    private ItemService itemService;
+
+    @PostMapping
+    public ItemDto createItem(final @RequestBody ItemDto itemDto,
+                              final @RequestHeader(name = "X-Sharer-User-Id") int userId) {
+        return null;
+    }
+
+    @PatchMapping("/{itemId}")
+    public ItemDto updateItem(final @PathVariable int itemId, final @RequestBody ItemDto itemDto,
+                              final @RequestHeader(name = "X-Sharer-User-Id") int userId) {
+        return null;
+    }
+
+    @GetMapping("/{itemId}")
+    public ItemDto getItemById(final @PathVariable int itemId) {
+        return null;
+    }
+
+    @GetMapping
+    public Collection<ItemDto> getAllUserItems(final @RequestHeader(name = "X-Sharer-User-Id") int userId) {
+        return null;
+    }
+
+    @GetMapping("/search")
+    public Collection<ItemDto> searchItem(final @RequestParam String text) {
+        return null;
+    }
 }
---
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -0,0 +1,8 @@
+package ru.practicum.shareit.item;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+
+}
