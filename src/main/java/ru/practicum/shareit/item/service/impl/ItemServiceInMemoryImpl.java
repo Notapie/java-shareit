25573d43fa1eeb9097d7d8cd6c429bb9c0b93d1b
notapie <notapie@yandex.ru>
2023-09-26 23:45:26 +0300

    feat: create item module struct

---
+++ src/main/java/ru/practicum/shareit/item/service/impl/ItemServiceInMemoryImpl.java
@@ -0,0 +1,9 @@
+package ru.practicum.shareit.item.service.impl;
+
+import org.springframework.stereotype.Service;
+import ru.practicum.shareit.item.service.ItemService;
+
+@Service
+public class ItemServiceInMemoryImpl implements ItemService {
+
+}
