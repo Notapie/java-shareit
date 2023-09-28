6ba8968c18ae9ca29155ec9c2d54e902fcca2eb1
notapie <notapie@yandex.ru>
2023-09-28 13:27:04 +0300

    fix: change response object in item controller

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.ItemRequestDto;
+import ru.practicum.shareit.item.dto.ItemResponseDto;
 import ru.practicum.shareit.item.service.ItemService;
 
 import java.util.Collection;
@@ -16,29 +17,29 @@ public class ItemController {
     private final ItemService itemService;
 
     @PostMapping
-    public ItemRequestDto createItem(final @RequestBody ItemRequestDto itemRequestDto,
-                                     final @RequestHeader(name = "X-Sharer-User-Id") int userId) {
+    public ItemResponseDto createItem(final @RequestBody ItemRequestDto itemRequestDto,
+                                      final @RequestHeader(name = "X-Sharer-User-Id") int userId) {
         return itemService.create(itemRequestDto, userId);
     }
 
     @PatchMapping("/{itemId}")
-    public ItemRequestDto updateItem(final @PathVariable int itemId, final @RequestBody ItemRequestDto itemRequestDto,
+    public ItemResponseDto updateItem(final @PathVariable int itemId, final @RequestBody ItemRequestDto itemRequestDto,
                                      final @RequestHeader(name = "X-Sharer-User-Id") int userId) {
         return itemService.update(userId, itemId, itemRequestDto);
     }
 
     @GetMapping("/{itemId}")
-    public ItemRequestDto getItemById(final @PathVariable int itemId) {
+    public ItemResponseDto getItemById(final @PathVariable int itemId) {
         return itemService.getById(itemId);
     }
 
     @GetMapping
-    public Collection<ItemRequestDto> getAllUserItems(final @RequestHeader(name = "X-Sharer-User-Id") int userId) {
+    public Collection<ItemResponseDto> getAllUserItems(final @RequestHeader(name = "X-Sharer-User-Id") int userId) {
         return itemService.getAllUserItems(userId);
     }
 
     @GetMapping("/search")
-    public Collection<ItemRequestDto> searchItem(final @RequestParam String text) {
+    public Collection<ItemResponseDto> searchItem(final @RequestParam String text) {
         return itemService.search(text);
     }
 }
