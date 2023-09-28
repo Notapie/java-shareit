db981a33ebdcd61d03e4788af58b283cfd253169
notapie <notapie@yandex.ru>
2023-09-28 03:29:12 +0300

    feat: implement error controller methods

--- src/main/java/ru/practicum/shareit/advice/ErrorController.java
+++ src/main/java/ru/practicum/shareit/advice/ErrorController.java
@@ -6,17 +6,37 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.advice.model.ErrorResponse;
-import ru.practicum.shareit.exception.AuthorizationException;
+import ru.practicum.shareit.exception.AlreadyExistsException;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.exception.ValidationException;
 
 @RestControllerAdvice
 @Slf4j
 public class ErrorController {
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ErrorResponse authorizationException(AuthorizationException e) {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse notFound(final NotFoundException e) {
         log.debug(e.getMessage());
         return new ErrorResponse(
-                "No authorization", e.getMessage()
+                "Not found", e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse alreadyExists(final AlreadyExistsException e) {
+        log.debug(e.getMessage());
+        return new ErrorResponse(
+                "Already exists", e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse validationError(final ValidationException e) {
+        log.debug(e.getMessage());
+        return new ErrorResponse(
+                "Validation error", e.getMessage()
         );
     }
 }
