2d574cc0bec93a9ea7c49eaf56c5668e13587265
notapie <notapie@yandex.ru>
2023-10-13 16:29:12 +0300

    feat: create entity on user model

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,12 +1,25 @@
 package ru.practicum.shareit.user.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
-@Data
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
 @Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     Integer id;
-    String name;
+
+    @Column(name = "email", nullable = false, length = 64, unique = true)
     String email;
+
+    @Column(name = "name", length = 64)
+    String name;
 }
