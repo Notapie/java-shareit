c3a9ccbebc886c96fd99f39fa2d1628bf2a62fe9
notapie <notapie@yandex.ru>
2023-10-15 22:50:45 +0300

    fix

--- src/main/java/ru/practicum/shareit/booking/repository/BookingJpaRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingJpaRepository.java
@@ -8,6 +8,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BookingJpaRepository extends JpaRepository<Booking, Integer> {
-    @Query("from Booking where endTime >= ?1 and startTime <= ?2 and status = Booking.Status.APPROVED")
+    @Query("from Booking where endTime >= ?1 and startTime <= ?2 and status = 'APPROVED'")
     List<Booking> findBookingsBetweenDates(LocalDateTime startTime, LocalDateTime endTime);
 }
