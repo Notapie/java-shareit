c4411305562cc6897fc94a9ecaf0b2e1faccb02d
notapie <notapie@yandex.ru>
2023-10-15 21:55:14 +0300

    feat: add booking object mapper

---
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingObjectMapper.java
@@ -0,0 +1,34 @@
+package ru.practicum.shareit.booking.dto;
+
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.dto.ItemObjectMapper;
+import ru.practicum.shareit.user.dto.UserObjectMapper;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class BookingObjectMapper {
+    public static Booking fromBookingRequestDto(BookingRequestDto bookingRequestDto) {
+        return Booking.builder()
+                .beginTime(bookingRequestDto.getStart())
+                .endTime(bookingRequestDto.getEnd())
+                .build();
+    }
+
+    public static BookingResponseDto toBookingResponseDto(Booking booking) {
+        return BookingResponseDto.builder()
+                .id(booking.getId())
+                .item(ItemObjectMapper.toItemResponseDto(booking.getItem()))
+                .booker(UserObjectMapper.toUserResponseDto(booking.getBooker()))
+                .start(booking.getBeginTime())
+                .end(booking.getEndTime())
+                .status(booking.getStatus())
+                .build();
+    }
+
+    public static Collection<BookingResponseDto> bookingResponseDto(Collection<Booking> bookings) {
+        return bookings.stream()
+                .map(BookingObjectMapper::toBookingResponseDto)
+                .collect(Collectors.toList());
+    }
+}
