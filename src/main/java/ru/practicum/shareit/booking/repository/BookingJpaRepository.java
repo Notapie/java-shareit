c42fb285d30be9c8ab009633fa849840831a6cd3
notapie <notapie@yandex.ru>
2023-10-15 22:49:26 +0300

    feat: add findBookingsBetweenDates query in booking jpa repository

--- src/main/java/ru/practicum/shareit/booking/repository/BookingJpaRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingJpaRepository.java
@@ -1,6 +1,13 @@
 package ru.practicum.shareit.booking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.booking.model.Booking;
 
-public interface BookingJpaRepository extends JpaRepository<Booking, Integer> {}
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface BookingJpaRepository extends JpaRepository<Booking, Integer> {
+    @Query("from Booking where endTime >= ?1 and startTime <= ?2 and status = Booking.Status.APPROVED")
+    List<Booking> findBookingsBetweenDates(LocalDateTime startTime, LocalDateTime endTime);
+}
