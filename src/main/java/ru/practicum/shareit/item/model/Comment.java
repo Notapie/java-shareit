1805368129241185e7d9ae7d1770dc1c422f60ce
notapie <notapie@yandex.ru>
2023-10-19 11:57:33 +0300

    feat: add add comment entity

---
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.item.model;
+
+import lombok.*;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "\"comment\"")
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @Column(name = "creation_time", nullable = false)
+    private LocalDateTime creationTime;
+}
