0b0aacee6c08720aae90c673f6731f84e19fc963
notapie <notapie@yandex.ru>
2023-10-19 14:20:37 +0300

    style

--- src/main/java/ru/practicum/shareit/item/repository/CommentJpaRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/CommentJpaRepository.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 import java.util.List;
 
 public interface CommentJpaRepository extends JpaRepository<Comment, Integer> {
-    List<Comment> findCommentsByItem_Id(int item_id);
+    List<Comment> findCommentsByItem_Id(int itemId);
+
     List<Comment> findCommentsByItem_IdIn(Collection<Integer> itemIds);
 }
