bad6862edb9f05c489457370d328c189299d74a4
notapie <notapie@yandex.ru>
2023-11-26 13:04:11 +0300

    feat: add IR endpoints in IR controller

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,20 +1,38 @@
 package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.IRRequestDto;
 import ru.practicum.shareit.request.dto.IRResponseDto;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping(path = "/requests")
 @RequiredArgsConstructor
 public class ItemRequestController {
-
     @PostMapping
-    public IRResponseDto addNewRequest() {
+    public IRResponseDto addNewRequest(@RequestBody IRRequestDto irRequestDto,
+                                       @RequestHeader(name = "X-Sharer-User-Id") int ownerId) {
         // TODO: add method body
         return null;
     }
 
+    @GetMapping
+    public Collection<IRResponseDto> getAllUserRequests(@RequestHeader(name = "X-Sharer-User-Id") int ownerId) {
+        // TODO: add method body
+        return  null;
+    }
+
+    @GetMapping("/all")
+    public Collection<IRResponseDto> getAllRequests(@RequestParam int from, @RequestParam int size) {
+        // TODO: add method body
+        return null;
+    }
+
+    @GetMapping("/{requestId}")
+    public IRResponseDto getRequest(@PathVariable int requestId) {
+        // TODO: add method body
+        return null;
+    }
 }
