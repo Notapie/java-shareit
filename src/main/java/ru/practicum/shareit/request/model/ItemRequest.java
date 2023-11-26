5d6cab257d03a45ac40291032c172470868bce5c
notapie <notapie@yandex.ru>
2023-11-26 14:03:49 +0300

    feat: add IR entity

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,7 +1,36 @@
 package ru.practicum.shareit.request.model;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.*;
+import ru.practicum.shareit.item.model.Item;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Entity
+@Table(name = "\"request\"")
+@Builder(toBuilder = true)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "created", nullable = false)
+    private LocalDateTime created;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "\"item_request\"",
+            joinColumns = { @JoinColumn(name = "request_id") },
+            inverseJoinColumns = { @JoinColumn(name = "item_id") }
+    )
+    private Collection<Item> items;
 }
