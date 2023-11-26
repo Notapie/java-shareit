d9227c51047335590492bee7d3d91fcd0aed7f2b
notapie <notapie@yandex.ru>
2023-11-26 09:04:31 +0300

    feat: add post Item request mapping, add IR response dto file

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,12 +1,20 @@
 package ru.practicum.shareit.request;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.request.dto.IRResponseDto;
 
-/**
- * TODO Sprint add-item-requests.
- */
 @RestController
 @RequestMapping(path = "/requests")
+@RequiredArgsConstructor
 public class ItemRequestController {
+
+    @PostMapping
+    public IRResponseDto addNewRequest() {
+        // TODO: add method body
+        return null;
+    }
+
 }
