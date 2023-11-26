09248781b78ef45befbcb53b7ed096986e962ece
notapie <notapie@yandex.ru>
2023-11-26 15:58:09 +0300

    feat: add IR object mapper

--- src/main/java/ru/practicum/shareit/request/dto/IRObjectMapper.java
+++ src/main/java/ru/practicum/shareit/request/dto/IRObjectMapper.java
@@ -1,22 +1,31 @@
 package ru.practicum.shareit.request.dto;
 
+import ru.practicum.shareit.item.dto.ItemObjectMapper;
 import ru.practicum.shareit.request.model.ItemRequest;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class IRObjectMapper {
     public static ItemRequest fromRequestDto(IRRequestDto irRequestDto) {
-        // TODO: add method body
-        return null;
+        return ItemRequest.builder()
+                .description(irRequestDto.getDescription())
+                .build();
     }
 
     public static IRResponseDto toResponseDto(ItemRequest itemRequest) {
-        // TODO: add method body
-        return null;
+        return IRResponseDto.builder()
+                .id(itemRequest.getId())
+                .description(itemRequest.getDescription())
+                .created(itemRequest.getCreated())
+                .items(ItemObjectMapper.toItemResponseDto(itemRequest.getItems()))
+                .build();
     }
 
     public static Collection<IRResponseDto> toResponseDto(Collection<ItemRequest> itemRequests) {
-        // TODO: add method body
-        return null;
+        return itemRequests.stream()
+                .map(IRObjectMapper::toResponseDto)
+                .collect(Collectors.toList());
     }
 }
