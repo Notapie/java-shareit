5328b79d4e9372f1d84cb9ce0980957907196df2
notapie <notapie@yandex.ru>
2023-11-26 15:48:48 +0300

    feat: add IR jpa repository

---
+++ src/main/java/ru/practicum/shareit/request/repository/ItemRequestJpaRepository.java
@@ -0,0 +1,6 @@
+package ru.practicum.shareit.request.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.practicum.shareit.request.model.ItemRequest;
+
+public interface ItemRequestJpaRepository extends JpaRepository<ItemRequest, Integer> {}
