832b6d6365921694e6b81394b9f5d8bb74c5a540
notapie <notapie@yandex.ru>
2023-11-29 05:31:03 +0300

    feat: add item request unit test

--- src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceUnitTest.java
+++ src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceUnitTest.java
@@ -1,5 +1,62 @@
 package ru.practicum.shareit.request.service;
 
-class ItemRequestServiceUnitTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.practicum.shareit.request.dto.IRRequestDto;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.request.repository.ItemRequestJpaRepository;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.service.impl.UserJpaUtil;
 
+import java.time.LocalDateTime;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class ItemRequestServiceUnitTest {
+    @Mock
+    ItemRequestJpaRepository irRepositoryMock;
+
+    @Mock
+    UserJpaUtil userUtilMock;
+
+    @Mock
+    ItemRequestJpaUtil irUtilMock;
+
+    @InjectMocks
+    ItemRequestService irService;
+
+    @Test
+    @DisplayName("should success add new item request")
+    public void shouldSuccessCreateNewItemRequest() {
+        final User owner = new User(1, "owner", "request owner");
+        final LocalDateTime currentTime = LocalDateTime.now();
+        final IRRequestDto irRequestDto = new IRRequestDto(
+                "request desc"
+        );
+        final ItemRequest expectedSavedIR = new ItemRequest(
+                1,
+                irRequestDto.getDescription(),
+                currentTime,
+                owner,
+                null
+        );
+
+        when(userUtilMock.requireFindById(owner.getId()))
+                .thenReturn(owner);
+        when(irRepositoryMock.save(any()))
+                .thenAnswer(invocationOnMock -> {
+                    final ItemRequest irParam = invocationOnMock.getArgument(0, ItemRequest.class);
+                    return irParam.toBuilder().id(expectedSavedIR.getId()).build();
+                });
+
+        final ItemRequest savedItemRequest = irService.createNew(irRequestDto, owner.getId());
+        Assertions.assertEquals(expectedSavedIR.getId(), savedItemRequest.getId());
+        Assertions.assertEquals(expectedSavedIR.getOwner().getId(), savedItemRequest.getOwner().getId());
+    }
 }
