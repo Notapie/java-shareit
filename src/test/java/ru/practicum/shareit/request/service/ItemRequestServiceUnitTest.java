9f0802e6680280934c4c6bb0360268f4c8700df9
notapie <notapie@yandex.ru>
2023-12-03 20:53:27 +0300

    feat: add IR unit tests

--- src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceUnitTest.java
+++ src/test/java/ru/practicum/shareit/request/service/ItemRequestServiceUnitTest.java
@@ -14,6 +14,7 @@ import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.service.impl.UserJpaUtil;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -31,22 +32,22 @@ class ItemRequestServiceUnitTest {
     @InjectMocks
     ItemRequestService irService;
 
+    final User owner = new User(1, "owner", "request owner");
+    final LocalDateTime currentTime = LocalDateTime.now();
+    final IRRequestDto irRequestDto = new IRRequestDto(
+            "request desc"
+    );
+    final ItemRequest expectedSavedIR = new ItemRequest(
+            1,
+            irRequestDto.getDescription(),
+            currentTime,
+            owner,
+            null
+    );
+
     @Test
     @DisplayName("should success add new item request")
     public void shouldSuccessCreateNewItemRequest() {
-        final User owner = new User(1, "owner", "request owner");
-        final LocalDateTime currentTime = LocalDateTime.now();
-        final IRRequestDto irRequestDto = new IRRequestDto(
-                "request desc"
-        );
-        final ItemRequest expectedSavedIR = new ItemRequest(
-                1,
-                irRequestDto.getDescription(),
-                currentTime,
-                owner,
-                null
-        );
-
         when(userUtilMock.requireFindById(owner.getId()))
                 .thenReturn(owner);
         when(irRepositoryMock.save(any()))
@@ -58,5 +59,39 @@ class ItemRequestServiceUnitTest {
         final ItemRequest savedItemRequest = irService.createNew(irRequestDto, owner.getId());
         Assertions.assertEquals(expectedSavedIR.getId(), savedItemRequest.getId());
         Assertions.assertEquals(expectedSavedIR.getOwner().getId(), savedItemRequest.getOwner().getId());
+    }
+
+    @Test
+    @DisplayName("should get all ir by owner")
+    public void getAllByOwner() {
+        when(irRepositoryMock.findItemRequestsByOwner_Id(owner.getId()))
+                .thenReturn(List.of(expectedSavedIR));
+
+        final ItemRequest result = irService.getAllByOwner(owner.getId()).iterator().next();
+
+        Assertions.assertEquals(owner.getId(), result.getOwner().getId());
+    }
+
+    @Test
+    @DisplayName("should get all ir")
+    public void getAll() {
+        when(irRepositoryMock.findAllItemRequestsExcludeOwner(anyInt(), any()))
+                .thenReturn(List.of(expectedSavedIR));
+
+        final ItemRequest result = irService.getAll(568, 0, 10).iterator().next();
+
+        Assertions.assertEquals(owner.getId(), result.getOwner().getId());
+        Assertions.assertEquals(expectedSavedIR.getId(), result.getId());
+    }
+
+    @Test
+    @DisplayName("should get ir by id")
+    public void getById() {
+        when(irUtilMock.requireFindById(expectedSavedIR.getId()))
+                .thenReturn(expectedSavedIR);
+
+        final ItemRequest result = irService.getById(465, expectedSavedIR.getId());
+
+        Assertions.assertEquals(expectedSavedIR.getId(), result.getId());
     }
 }
